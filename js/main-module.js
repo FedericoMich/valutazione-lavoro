9c5adacd0dba289a30e1c42fb0b4476d6473954b
federico <federico.mich97@gmail.com>
2021-05-21 17:20:49 +0200

    V 2.0

--- js/main-module.js
+++ js/main-module.js
@@ -1,7 +1,26 @@
+export const searchFunction = () => {
+    domainExtraction();
+    pathExtraction();
+    schemeExtraction();
+}
 
-export const SearchFunction = () => {
+const domainExtraction = () => {
     let URL = document.getElementById('findtext').value;
     let res = URL.split("/", 1);
-    console.log(res);
     document.getElementById("textDomain").innerHTML = res;
+}
+
+
+const schemeExtraction = () => {
+    let URL = document.getElementById('findtext').value;
+    document.getElementById("textScheme").innerHTML = URL;
+
+}
+
+const pathExtraction = () => {
+    let URL = document.getElementById('findtext').value;
+    let res = URL.substring(URL.indexOf('/') + 1);
+    document.getElementById("textPath").innerHTML = res;
+
+
 }
