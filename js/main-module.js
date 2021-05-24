cefbeb24563a8627444833ed6a2af78f514ae1dd
federico <federico.mich97@gmail.com>
2021-05-24 21:00:45 +0200

    Version 2.1

--- js/main-module.js
+++ js/main-module.js
@@ -2,6 +2,7 @@ export const searchFunction = () => {
     domainExtraction();
     pathExtraction();
     schemeExtraction();
+    fetchcHttp();
 }
 
 const domainExtraction = () => {
@@ -10,17 +11,29 @@ const domainExtraction = () => {
     document.getElementById("textDomain").innerHTML = res;
 }
 
-
 const schemeExtraction = () => {
-    let URL = document.getElementById('findtext').value;
-    document.getElementById("textScheme").innerHTML = URL;
-
+    document.getElementById("textScheme").innerHTML = "HTTP";
 }
 
 const pathExtraction = () => {
     let URL = document.getElementById('findtext').value;
     let res = URL.substring(URL.indexOf('/') + 1);
     document.getElementById("textPath").innerHTML = res;
+}
 
-
+const fetchcHttp = () => {
+    fetch('https://run.mocky.io/v3/2883afdb-7c4f-4dad-bb72-5183e5a0c599', {
+    })
+        .then(response => response.json())
+        .then(result => {
+            document.getElementById("ResponseTextVersion").innerHTML = ("Version: " + result.response.version);
+            document.getElementById("ResponseTextData").innerHTML = ("Date: " + result.response.data);
+            document.getElementById("ResponseTextServer").innerHTML = ("Server: " + result.response.server);
+            document.getElementById("ResponseTextLocation").innerHTML = ("Location: " + result.response.location);
+            document.getElementById("ResponseTextServer2").innerHTML = ("Server: " + result.response.server);
+            document.getElementById("ResponseTextVersion2").innerHTML = ("Version: " + result.response.version);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
